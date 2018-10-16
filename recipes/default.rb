ccfba3696115434bc39830ffa5e805c9f197e30e
Aymen Chetoui <achetoui@zendesk.com>
2018-10-16 22:44:00 +0200

    Install apache

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+package 'Install Apache' do
+  case node[:platform]
+  when 'redhat', 'centos'
+    package_name 'httpd'
+  when 'ubuntu', 'debian'
+    package_name 'apache2'
+  end
+end
--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,14 +5,13 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
+case os.family
+when 'redhat', 'centos'
+  describe package('httpd') do
+    it { should be_installed }
   end
-end
-
-# This is an example test, replace it with your own test.
-describe port(80), :skip do
-  it { should_not be_listening }
+when 'ubuntu', 'debian'
+  describe package('apache2') do
+    it { should be_installed }
+  end
 end
